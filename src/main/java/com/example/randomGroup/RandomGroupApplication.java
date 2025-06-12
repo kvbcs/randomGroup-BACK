06a3e80f9d43b4ad22a03735b1d2270d7ff70139
Kyllian <kyllianvibancos2@gmail.com>
2025-06-12 10:29:08 +0200

    project setup, added user model

---
+++ src/main/java/com/example/randomGroup/RandomGroupApplication.java
@@ -0,0 +1,13 @@
+package com.example.randomGroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomGroupApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/randomGroup/repository/randomGroupRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.example.randomGroup.repository;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class randomGroupRepository {
+
+}
