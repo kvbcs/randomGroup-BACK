af1fefaf7646d57d1238f246782bfa869d6728da
Kyllian <kyllianvibancos2@gmail.com>
2025-06-12 14:52:08 +0200

    finished student crud

---
+++ src/main/java/com/example/randomGroup/controller/StudentController.java
@@ -0,0 +1,56 @@
+package com.example.randomGroup.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.randomGroup.model.Student;
+import com.example.randomGroup.repository.StudentRepository;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentRepository repository;
+
+    public StudentController(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @PostMapping
+    public Student create(@RequestBody Student student) {
+        return repository.save(student);
+    }
+
+    @PutMapping("/{id}")
+    public Student update(@PathVariable Long id, @RequestBody Student student) {
+        Student existingStudent = repository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        existingStudent.setName(student.getName());
+        existingStudent.setGender(student.getGender());
+        existingStudent.setFrLevel(student.getFrLevel());
+        existingStudent.setIsDWWM(student.getIsDWWM());
+        existingStudent.setSkillLevel(student.getSkillLevel());
+        existingStudent.setProfile(student.getProfile());
+        existingStudent.setAge(student.getAge());
+       
+        return repository.save(existingStudent);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        if (repository.findById(id).isEmpty()) {
+            System.out.print("Unexisting user !");
+        }
+        repository.deleteById(id);
+    }
+}
